f38d6c8e32bbea2161899fadb50bad988dcdd244
folathecoder <folathecoder@gmail.com>
2023-04-22 13:37:39 +0100

    Initial Commit

---
+++ QuickBite App/QuickBite_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickBite_AppApp.swift
+//  QuickBite App
+//
+//  Created by Folarin Akinloye on 22/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct QuickBite_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
