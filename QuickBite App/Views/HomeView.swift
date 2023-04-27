635fedd0aa6b7223e099708266fc491e21084ae2
folathecoder <folathecoder@gmail.com>
2023-04-27 16:12:18 +0100

    Implemented ad banner component

--- QuickBite App/Views/HomeView.swift
+++ QuickBite App/Views/HomeView.swift
@@ -7,10 +7,55 @@
 
 import SwiftUI
 
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 97.97, height: 43)
+            .background(Color(#colorLiteral(red: 1, green: 0.7569, blue: 0.1804, alpha: 1)))
+            .cornerRadius(15)
+            .foregroundColor(.black)
+            .fontWeight(.bold)
+            .font(.system(size: 14))
+            
+    }
+}
+
 struct HomeView: View {
     var body: some View {
-        Text("This is the home view")
-        Text("Added another text name")
+        ScrollView {
+            ZStack {
+                Color.red
+                HStack{
+                    Image("BannerAdImage")
+                        .resizable()
+                        .frame(width: 122, height: 122.96)
+                        .padding(.trailing, 15)
+                    VStack(alignment: .leading) {
+                        Text("50% off special deal in May")
+                            .font(.system(size: 16))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                        Button(action: {
+                                   print("Button tapped!")
+                               }) {
+                                   Text("Buy now")
+                               }
+                               .buttonStyle(CustomButtonStyle())
+                    }
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity, minHeight: 134)
+                .background(
+                      Image("BannerBackground")
+                          .resizable()
+                          .aspectRatio(contentMode: .fill)
+                          .edgesIgnoringSafeArea(.all)
+                )
+                .cornerRadius(16)
+            }
+        }
+        .padding()
     }
 }
 
