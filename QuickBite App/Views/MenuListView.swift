b26f2b6183b2f63df0e989d6c5e723dc2a3bc929
BabaOlumoh <olumohbabatunde@gmail.com>
2023-04-30 21:40:42 +0100

    First grid completed

--- QuickBite App/Views/MenuListView.swift
+++ QuickBite App/Views/MenuListView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-//let FoodList:  = <#initializer#>FoodItems
 //
 private var data: [Int] = Array(1...20)
 
@@ -14,6 +13,8 @@ private let adaptiveColumns = [
     GridItem(.adaptive(minimum: 340))
 ]
 struct MenuListView: View {
+    
+    //let FoodList: FoodItems
     @State var searchText = ""
     
     
@@ -43,24 +44,32 @@ struct MenuListView: View {
         .frame(width: 350)
         .accentColor(.black)
     
+        // List Array
+        
+        FoodGrid
+        
+        
+        
+        
+        
+        
 //List Example
         
                 //List
-                VStack{
-                    NavigationView{
-                        ScrollView{
-                            LazyVGrid(columns: adaptiveColumns, spacing:20) {
-                                ForEach(data, id: \.self) { number in
-                                    ZStack{
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .frame(width: 340, height: 280)
-                                    }
-        
+        VStack{
+            NavigationView{
+                    ScrollView{
+                        LazyVGrid(columns: adaptiveColumns, spacing:10) {
+                            ForEach(data, id: \.self) { number in
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 340, height: 280)
                                 }
-                            }//LazyVGrid
-                        }//ScrollView
-                    }//Navigation
-                }//VStack
+                            }
+                        }//LazyVGrid
+                    }//ScrollView
+                }//Navigation
+            }//VStack
         
         
         
@@ -135,6 +144,53 @@ extension MenuListView {
             Button(action:{}, label: {Image(systemName: "slider.horizontal.3")})
             
             
+        }
+    }
+    
+    private var FoodGrid: some View {
+        VStack( spacing: 10){
+            Image("Burger")
+                .resizable()
+                .frame(width: 333, height: 154)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius:5)
+            
+                .overlay(alignment: .topTrailing, content: {
+                    Text("£12.20")
+                        .background(
+                            Capsule()
+                                .fill(Color(.white))
+                        )
+                        .padding()
+                })
+            
+        VStack(alignment: .leading, spacing: 5){
+                Text("Burger")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Delicious and tasty cheese burger one of tasty town’s finest, it has a record sale of 2000 orders and a rating of 4.8 since it’s introduction.")
+                    .font(.body)
+                    .fontWeight(.light)
+                .frame(width: 333, height: 119)
+                .shadow(radius:5)
+            
+                .overlay(alignment: .bottomTrailing, content: {
+                    HStack {
+                        HStack {
+                            Image("CountDown")
+                                .resizable()
+                                .frame(width: 31.68, height: 26)
+                            Text("1")
+                                .font(.body)
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.red)
+                            Image("CountUp")
+                                .resizable()
+                                .frame(width: 31.68, height: 26)
+                        }
+                    }
+                })
+            }
         }
     }
 }
